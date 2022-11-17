5229c318b8b5c16a2b01e28ea316d4a6ade89cb2
bharti-bhardwaj <bharti.sharma_cs19@gla.ac.in>
2022-11-18 04:12:24 +0530

    difference b/w String literal and String object

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+System.out.println("Hello world!")
         System.out.println("Hello world!");
+        String str1 = "bharti"; //create a string by using a string literal
+        String str2 = new String("Bharti"); // create a string by using the string object
+        System.out.println(str1==str2);(difference b/w String literal and String object)
     }
 }
